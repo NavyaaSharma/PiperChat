f09991877a2b7fa74ae99ceec633fa5cd78ad7f5
VishaalSelvaraj2000 <vishaals2000@gmail.com>
2019-03-23 01:20:52 +0530

    Added UI for encrypted text messaging.

---
+++ PiperChat/MainPage.xaml.vb
@@ -0,0 +1,7 @@
+﻿Class MainPage
+    Private Sub Page_Loaded(sender As Object, e As RoutedEventArgs)
+        Dim item As New ListViewItem With {.Content = "Nick Fury: Sending Message to Captain Marvel"}
+        item.Style = CType(My.Application.Resources("ListViewItemWinterStyle.Input"), Style)
+        listView.Items.Add(item)
+    End Sub
+End Class
